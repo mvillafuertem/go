bc05b6608d323119ced0dca286c287030774c5b2
Miguel <mivimur@hotmail.com>
2019-01-05 19:46:14 +0100

    feature(Channel) : add example

--- domain/operations.go
+++ domain/operations.go
@@ -3,28 +3,28 @@ package domain
 import log "github.com/sirupsen/logrus"
 
 type Operations interface {
-	Add(... int) int
-	Subtract(... int) int
+	Add(*int, ... int)
+	Subtract(*int, ... int)
 }
 
 
 type OperationsService struct {}
 
-func (o OperationsService) Add(nums... int) (r int){
+func (o OperationsService) Add(r *int, nums... int){
 
 	for n := range nums {
-		log.Debugf("%d add %d", r, n)
-		r += nums[n]
+		log.Debugf("%d add %d", *r, nums[n])
+		*r += nums[n]
 	}
 
 	return
 }
 
-func (o OperationsService) Subtract(nums... int) (r int) {
+func (o OperationsService) Subtract(r *int, nums... int) {
 
 	for n := range nums {
 		log.Debugf("%d subtract %d", r, n)
-		r -= nums[n]
+		*r -= nums[n]
 	}
 
 	return
--- main.go
+++ main.go
@@ -2,23 +2,68 @@ package main
 
 import (
 	"fmt"
-	"go/application"
+	log "github.com/sirupsen/logrus"
 	"go/domain"
-	"sync"
+	"math/rand"
+	"time"
 )
 
 func main() {
 
-	service := domain.OperationsService{}
-	routines := application.GoRoutines{Operations: service}
+	channel1 := make(chan int, 1)
+	channel2 := make(chan int, 1)
 
-	(&sync.WaitGroup{}).Add(1)
+	go Producer(channel1)
+	go Producer(channel2)
 
-	go routines.AddRoutine(1, 2, 3, 4, 5)
-	go routines.AddRoutine(10, 1, 2, 3, 4)
-	go routines.SubtractRoutine(10, 1, 2, 3, 4)
 
-	(&sync.WaitGroup{}).Wait()
+	go ConsumerAdd(channel1)
+	go ConsumerAdd(channel1)
 
-	fmt.Println()
+	go ConsumerSubtract(channel2)
+	go ConsumerSubtract(channel2)
+
+	//for {
+	//	select {
+	//	case <- channel1:
+	//		go ConsumerAdd(channel1)
+	//		time.Sleep(time.Second * 1)
+	//	case <-channel2:
+	//		go ConsumerSubtract(channel2)
+	//		time.Sleep(time.Second * 1)
+	//	}
+	//}
+
+	var input string
+	fmt.Scanln(&input)
+
+}
+
+func Producer(producer chan<- int) {
+	for {
+		n := rand.Intn(100)
+		log.Info("[PRODUCER] Se producen datos ", n)
+		producer <- n
+	}
+}
+
+func ConsumerAdd(consumer <-chan int) {
+	var r int
+	for {
+		n := <-consumer
+		domain.OperationsService{}.Add(&r, n)
+		log.Info("[CONSUMER ADD] Se consumen datos ", n, r)
+		time.Sleep(time.Second * 1)
+
+	}
+}
+
+func ConsumerSubtract(consumer <-chan int) {
+	var r int
+	for {
+		n := <-consumer
+		domain.OperationsService{}.Subtract(&r, n)
+		log.Info("[CONSUMER SUBTRACT] Se consumen datos ", n, r)
+		time.Sleep(time.Second * 1)
+	}
 }
